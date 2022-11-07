32f43cb705b501dd0d23c5b24886731351144110
nuranawsaf <nuranawsaf712@gmail.com>
2022-11-07 19:40:51 +0600

    dsd

--- src/components/home/modal/Modal.tsx
+++ src/components/home/modal/Modal.tsx
@@ -107,13 +107,13 @@ function Modal() {
   }, [db, movie?.id])
 
   // Check if the movie is already in the user's list
-  useEffect(
-    () =>
-      setAddedToList(
-        movies.findIndex((result) => result.data().id === movie?.id) !== -1
-      ),
-    [movies]
-  )
+  // useEffect(
+  //   () =>
+  //     setAddedToList(
+  //       movies.findIndex((result) => result.data().id === movie?.id) !== -1
+  //     ),
+  //   [movies]
+  // )
 
   const handleList = async () => {
     if (addedToList) {
